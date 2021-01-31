d29ce9a821935cb31f2018142384abcdf77267f8
acanofair <malik.owten@gmail.com>
2021-01-31 15:38:36 -0600

    BS4

--- Scrape.py
+++ Scrape.py
@@ -1,11 +1,12 @@
 import re
 import urllib.request 
-with urllib.request.urlopen('http://python.org') as response: 
-    html = response.read()
-    html_decoded = html.decode("utf-8")
-
+# with urllib.request.urlopen as response
+from urllib.request import urlopen as open
 
 def test_scrape(): 
+    url = 'http://python.org'
+    html = open(url).read()
+    html_decoded = html.decode("utf-8")
     title_index = html_decoded.find("<title>")
 
     start_index = title_index + len("<title>")
@@ -17,7 +18,35 @@ def test_scrape():
     return title 
 
 
-print(str(test_scrape()))
+def scrape_work():
+    url = 'http://python.org'
+    html = open(url).read()
+    html_decoded = html.decode("utf-8")
+    pattern = ("<title.*?>.*?</title.*?>")
+    match_results = re.search(pattern, html_decoded, re.IGNORECASE)
+    title = match_results.group()
+    title = re.sub("<.*?>", "", title) # Removes HTML TAGS FROM TITLE 
+    return title 
+
+
+def nameandcolor_scrape():
+    url = "http://olympus.realpython.org/profiles/dionysus"
+    html = open(url).read().decode("utf-8")
+    patterns = ["Name:" ,"Favorite Color: "]
+    for string in patterns: 
+        start = html.find(string) + len(string)
+        tag = start + html[start:].find("<") 
+        texts  = html[start : tag]
+        print (texts)
+        
+def soupscrape():
+    from bs4 import Beau
+    from urllib.request import urlopen 
+    url = "http://python.org"
+
+
+
+
 
 
 
