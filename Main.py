ba46e2842b40f60c0172fbb4bfeeb564236cd79a
acanofair <malik.owten@gmail.com>
2021-02-11 23:32:01 -0600

    Testing Async

--- Main.py
+++ Main.py
@@ -1,20 +1,31 @@
 import discord
 import os
+import asyncio
 from discord.ext import commands 
 
 
-client = commands.Bot(command_prefix= '.')
 
-@client.event
-async def on_ready():
-    print('New participant has entered!'.format(client))
+def DISORD():
+    client = commands.Bot(command_prefix= '.')
+    @client.event
+    async def on_ready():
+        print('New participant has entered!'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
 
-    if message.content.startswith('Jin Kazama'):
-        await message.channel.send('Welcome to The King of Iron First Tournament!')
+        if message.content.startswith('Jin Kazama'):
+            await message.channel.send('Welcome to The King of Iron First Tournament!')
 
-client.run(os.environ.get('DISCORD'))
+    client.run(os.environ.get('DISCORD'))
+
+
+
+async def eyesoverheaven():
+    
+    
+    return 0 
+
+
